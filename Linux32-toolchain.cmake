9e2dd1b3231c1d7ddde41e057c8b397840e64a20
Unknown <robots_only_adrido@gmx.com>
2019-04-02 17:42:14 +0200

    Fix x86-linux builds

--- Linux32-toolchain.cmake
+++ Linux32-toolchain.cmake
@@ -2,8 +2,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CPACK_SYSTEM_NAME Linux32)
 
-message(STATUS "BUILDING x86-Linux")
-
-# Which compilers to use for C and C++
-set(CMAKE_C_COMPILER gcc -m32)
-set(CMAKE_CXX_COMPILER g++ -m32)
+# which compilers to use for C and C++
+#set(CMAKE_C_COMPILER gcc)
+set(CMAKE_C_FLAGS -m32)
+#set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_CXX_FLAGS -m32)
